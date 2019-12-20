205dd2e9bd293df92256e2ab95bdb521ebea4c2d
yeyuesama <643639230@qq.com>
2019-12-20 19:40:55 +0800

    init repo

--- src/main/java/sama/dome/community/community/controller/indexController.java
+++ src/main/java/sama/dome/community/community/controller/indexController.java
@@ -7,10 +7,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class Hello {
-    @GetMapping("/hello")
+    @GetMapping("/")
      public String hello(@RequestParam(name = "name")String name, Model model){
         model.addAttribute("name",name);
-        return "hello";
+        return "index";
      }
 
 }
